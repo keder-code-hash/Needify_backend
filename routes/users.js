5f9e7f50799588ba041844aa0206bc1c51133588
rajib <rjibnasipuri@gmail.com>
2022-03-13 21:50:56 +0530

    sign up login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const usersRouter = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+usersRouter.
+  route('/')
+  .get(getUser)
+  .post(postUser)
+  .patch(updateUser)
+  .delete(deleteUser)
 
-module.exports = router;
+function getUser(req, res) {
+  res.send(users)
+}
+function postUser(req, res) {
+  console.log(req.body);
+  users = req.body
+  res.json({
+    message: "data recived",
+    user: req.body
+  });
+}
+function updateUser(req, res) {
+  console.log(req.body);
+  let dataToBeUpdated = req.body;
+  for (key in dataToBeUpdated) {
+    users[key] = dataToBeUpdated[key]
+  }
+  res.json({
+    message: " data updated successfully",
+
+  })
+}
+function deleteUser(req, res) {
+  console.log(req.body)
+  users = {}
+  res.json({
+    message: "data deleted"
+  })
+}
+
+module.exports = usersRouter;
