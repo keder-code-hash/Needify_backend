d4f868c6157b8030290f8cd330d42aa9386c98bb
testKeder <keder.nath.mallick.2019@cse.tict.edu.in>
2022-03-16 23:23:35 +0530

    route details added

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,4 +1,3 @@
-const mongoose=require('mongoose') 
 const AdminBro = require('admin-bro')
 const AdminBroExpressjs = require('admin-bro-expressjs') 
 const res=require('@admin-bro/mongoose')
@@ -13,17 +12,6 @@ const ProductModel = require('../models/product')
 const RequirementsModel=require('../models/Requirements')
 const DonationModel = require('../models/DonationAmount')
 const UserRoleModel = require('../models/UserRoles')
-const ResourceModel = require('../models/UserResources')
-
-
-const mongooseDb = mongoose.connect('mongodb+srv://rajib005:rajib@cluster0.wkthe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true })
-
-
-
-const contentParent = {
-        name: 'content',
-        icon: 'Accessibility',
-}
 
 const adminBro = new AdminBro({ 
     resources: [
---
+++ routes/NGODetails.js
@@ -0,0 +1,13 @@
+/**
+ * 1. insert / update all details of any NGO details
+ * 2. add/update any member details
+ * 3. add/update any achievements details
+ * 4. add/update any events details
+ */
+
+// {
+//     ngodetails:{},
+//     achievements:[{}],
+//     events:[{}],
+//     members:[{}]
+// }
