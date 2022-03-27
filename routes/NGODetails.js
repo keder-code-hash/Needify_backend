a9f3aed75334776cf107907c7255f1ab4a260ef9
rajib <rjibnasipuri@gmail.com>
2022-03-27 11:09:11 +0530

    ngoRouter

--- routes/NGODetails.js
+++ routes/NGODetails.js
@@ -3,7 +3,9 @@
  * 2. add/update any member details
  * 3. add/update any achievements details
  * 4. add/update any events details
- */
+ */ 
+//api
+    
 
 // {
 //     ngodetails:{},
@@ -11,3 +13,25 @@
 //     events:[{}],
 //     members:[{}]
 // }
+const express = require('express');
+const NgoModel = require('../models/Ngo');
+const ngoRouter = require('../models/Ngo');
+
+
+ngoRouter.route('./ngodetails')
+.get(getNgodetails)
+.post(postNgodetails)
+
+function getNgodetails(req, res){
+    res.send(ngoUser)
+}
+
+async function postNgodetails(req, res){
+    let data = req.body();
+    let ngoUser = await NgoModel.create(data);
+    res.json({
+        message: 'ngo details created'
+    })
+}
+
+module.exports = ngoRouter
